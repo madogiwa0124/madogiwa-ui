4025550e3325fdf0d3c9d40d216e9709fe910a9e
madogiwa0124 <madogiwa0124@gmail.com>
2025-09-07 21:25:03 +0000

    fixed css build script by vite.

---
+++ packages/css/vite.config.ts
@@ -0,0 +1,52 @@
+import { type Plugin, defineConfig } from "vite";
+import postcssPresetEnv from "postcss-preset-env";
+import { globSync, rmSync } from "node:fs";
+
+const isProduction = process.env["NODE_ENV"] === "production";
+
+const removeFilePlugin = (glob: string, silent: boolean = false): Plugin => {
+  return {
+    name: "remove-file",
+    closeBundle() {
+      if (isProduction) {
+        try {
+          const files = globSync(glob);
+          for (const file of files) {
+            rmSync(file, { force: true });
+            // eslint-disable-next-line no-console
+            if (!silent) console.info(`Removed file: ${file}`);
+          }
+          if (files.length === 0 && !silent) {
+          // eslint-disable-next-line no-console
+            console.info(`No files matched pattern: ${glob}`);
+          }
+        }
+        catch (error) {
+        // eslint-disable-next-line no-console
+          console.error(`Error removing files with pattern ${glob}:`, error);
+        }
+      }
+    },
+  };
+};
+
+export default defineConfig({
+  plugins: [removeFilePlugin("dist/*.js")],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "main",
+      fileName: "madoginawa-ui.min",
+      formats: ["es"],
+    },
+    cssCodeSplit: false,
+    emptyOutDir: true,
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv(),
+      ],
+    },
+  },
+});
