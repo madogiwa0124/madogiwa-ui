a94e419feb899d379af0d1a1ceb9ef7f503c224b
madogiwa0124 <madogiwa0124@gmail.com>
2025-09-08 21:25:52 +0000

    fix typo.

--- packages/css/vite.config.ts
+++ packages/css/vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     lib: {
       entry: "src/index.ts",
       name: "main",
-      fileName: "madoginawa-ui.min",
+      fileName: "madogiwa-ui.min",
       formats: ["es"],
     },
     cssCodeSplit: false,
