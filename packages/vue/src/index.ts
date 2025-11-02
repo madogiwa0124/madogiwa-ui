2482b30826c44f05bacc919808794ba6299c7b64
madogiwa0124 <madogiwa0124@gmail.com>
2025-11-02 13:37:30 +0000

    fixed dir path for MHeading.

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,5 +1,5 @@
 export { MButton } from "./components/MButton";
 export { MNavbar, MNavbarItem, MNavbarHamburger, MNavbarHamburgerMenu, MNavbarTitle } from "./components/MNavbar";
 export { MContainer } from "./layouts/MContainer";
-export { MH1, MH2, MH3, MH4, MH5, MH6 } from "./MHeading";
+export { MH1, MH2, MH3, MH4, MH5, MH6 } from "./components/MHeading";
 export { MTabs, MTabsList, MTabsItem } from "./components/MTabs";
